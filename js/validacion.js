3390ec885b435240c75f3775838d8b79be373ad4
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-08-14 18:55:51 -0300

    validation email

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,31 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+document.getElementById('regBtn').addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+
+    const existingError = document.querySelector('.error-message');
+    if (existingError) {
+        existingError.remove();
+    }
+
+    if (!emailValidation(email)) {
+        const errorMessage = document.createElement('div');
+        errorMessage.textContent = 'Correo no válido';
+        errorMessage.classList.add('error-message', 'text-danger');
+
+        emailInput.insertAdjacentElement('afterend', errorMessage);
+    } else {
+        showAlertSuccess();
+    }
+});
+
+function emailValidation(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+}
